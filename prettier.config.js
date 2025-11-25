576fe160ec9c3db1e8477e8e0f0379bdb0308504
Vladimir Eremia <vladimireremia377@gmail.com>
2025-11-25 20:26:03 +0200

    chore(prettier): move prettier config to standalone file
    - Removed inline Prettier configuration from `package.json`.
    - Included HTML-specific parser options in the config.

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,12 @@ import personalConfig from '@evalore/prettier-config';
 export default {
   ...personalConfig,
   singleAttributePerLine: false,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        parser: 'angular',
+      },
+    },
+  ],
 };
