9360e270982e9cf7d1bfc3745f3eec232f371993
Vladimir Eremia <vladimireremia377@gmail.com>
2025-11-27 02:48:24 +0200

    feat(movie-list): enhance loading state and restore infinite scroll logic
    - Added loading indicator to the `MovieList` template for improved user feedback.
    - Restored and refactored infinite scroll functionality with optimized debounce and filtering.
    - Removed unused code and enhanced logic for detecting proximity to the page's bottom.

--- src/app/features/movie-list/movie-list.ts
+++ src/app/features/movie-list/movie-list.ts
@@ -3,15 +3,16 @@ import { MovieCard } from './movie-card/movie-card';
 import { Route } from '../../core/route/route';
 import { MoviesStore } from '../../core/stores/movies-store';
 import { MovieStore } from '../../core/stores/movie-store';
-import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, Subject } from 'rxjs';
 import { MovieCardSkeleton } from './movie-card/movie-card-skeleton/movie-card-skeleton';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-movie-list',
   imports: [MovieCard, MovieCardSkeleton],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.css',
-  // host: { '(window:scroll)': 'onScroll()' },
+  host: { '(window:scroll)': 'onScroll()' },
 })
 export class MovieList {
   public readonly moviesStore = inject(MoviesStore);
@@ -21,31 +22,30 @@ export class MovieList {
 
   private readonly scrollSubject = new Subject<void>();
 
-  // constructor() {
-  //   this.scrollSubject
-  //     .pipe(
-  //       map(() => this.isNearBottom()),
-  //       distinctUntilChanged(),
-  //       debounceTime(1000),
-  //       takeUntilDestroyed(this.destroyRef),
-  //     )
-  //     .subscribe((isNearBottom) => {
-  //       if (isNearBottom && !this.moviesStore.isLoading()) {
-  //         this.moviesStore.loadMoreMovies();
-  //       }
-  //     });
-  // }
-  //
-  // public onScroll(): void {
-  //   this.scrollSubject.next();
-  // }
-  //
-  // private isNearBottom(): boolean {
-  //   const scrollPosition = window.innerHeight + window.scrollY;
-  //   const documentHeight = document.documentElement.scrollHeight;
-  //   const threshold = 200;
-  //   return scrollPosition >= documentHeight - threshold;
-  // }
+  constructor() {
+    this.scrollSubject
+      .pipe(
+        map(() => this.isNearBottom()),
+        distinctUntilChanged(),
+        debounceTime(1000),
+        filter((isNearBottom) => isNearBottom && !this.moviesStore.isLoading()),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe(() => {
+        this.moviesStore.loadMoreMovies();
+      });
+  }
+
+  public onScroll(): void {
+    this.scrollSubject.next();
+  }
+
+  private isNearBottom(): boolean {
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const documentHeight = document.documentElement.scrollHeight;
+    const threshold = 200;
+    return scrollPosition >= documentHeight - threshold;
+  }
 
   protected navigateToMovie(movieId: string): void {
     this.movieStore.movieId.set(movieId);
